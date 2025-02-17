56c2c96a68820a5d39f00b62ed0b8fb805cd192a
md abu bokor siddik <mdabubokorsiddikbissness@gmail.com>
2025-02-17 21:19:19 +0600

    aded folder

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,12 @@
+/* eslint-disable prettier/prettier */
 import {heroui} from "@heroui/theme"
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    // eslint-disable-next-line prettier/prettier
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
     "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
