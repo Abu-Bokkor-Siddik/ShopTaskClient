ee92e88f489a0af8f6f519651f74ab754c0d1ba4
md abu bokor siddik <mdabubokorsiddikbissness@gmail.com>
2025-02-17 23:27:20 +0600

    uper content

--- src/components/autoComplete.tsx
+++ src/components/autoComplete.tsx
@@ -1,58 +1,33 @@
 /* eslint-disable prettier/prettier */
 "use client";
-import { Autocomplete, AutocompleteItem } from "@heroui/autocomplete";
 
-export const animals = [
-  {
-    label: "Cat",
-    key: "cat",
-    description: "The second most popular pet in the world",
-  },
-  {
-    label: "Dog",
-    key: "dog",
-    description: "The most popular pet in the world",
-  },
-  {
-    label: "Elephant",
-    key: "elephant",
-    description: "The largest land animal",
-  },
-  { label: "Lion", key: "lion", description: "The king of the jungle" },
-  { label: "Tiger", key: "tiger", description: "The largest cat species" },
-  { label: "Giraffe", key: "giraffe", description: "The tallest land animal" },
-];
 
 export default function Auto() {
   return (
-    // <div className="flex w-20 bg-neutral-700 mr-8 flex-wrap md:flex-nowrap ">
-    //   <Autocomplete className="max-w-xs" label="Eng">
-    //     {animals.map((animal) => (
-    //       <AutocompleteItem key={animal.key}>{animal.label}</AutocompleteItem>
-    //     ))}
-    //   </Autocomplete>
-
-    // </div>
-    <div className="flex justify-center items-center ">
-      <div >
-      <select defaultValue="eng" className="select bg-neutral-700 select-bordered mr-10 w-24 text-neutral-300">
+   <div>
+     <div className="flex justify-center items-center ">
+      
+      <select defaultValue="eng" className="select bg-neutral-700 select-bordered w-28 text-neutral-300">
         <option disabled selected>
-        Select language
+        Select
         </option>
         <option>Eng</option>
         <option>Bangla</option>
       </select>
-      </div>
+      
       {/*  */}
-      <div>
-      <select defaultValue="eng" className="select bg-neutral-700 select-bordered mr-10 w-24 text-neutral-300">
+     
+      <select defaultValue="eng" className="select bg-neutral-700 select-bordered w-28 text-neutral-300">
         <option disabled selected>
-        Select language
+        Select 
         </option>
         <option>Eng</option>
         <option>Bangla</option>
       </select>
-      </div>
+       <div className="mx-7 text-white">|</div>
+       <button  className="btn btn-neutral mx-3 bg-neutral-700 text-neutral-300">Sign In / Sign Up</button>
     </div>
+    {/* <h1>|</h1> */}
+   </div>
   );
 }
