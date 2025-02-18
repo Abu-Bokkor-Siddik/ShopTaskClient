6f300e591549eaeb6cb5c91652f9c0475e52659c
md abu bokor siddik <mdabubokorsiddikbissness@gmail.com>
2025-02-18 22:19:38 +0600

    uper content

--- src/components/autoComplete.tsx
+++ src/components/autoComplete.tsx
@@ -1,33 +1,39 @@
 /* eslint-disable prettier/prettier */
 "use client";
 
-
 export default function Auto() {
   return (
-   <div>
-     <div className="flex justify-center items-center ">
-      
-      <select defaultValue="eng" className="select btn-sm bg-neutral-700 select-bordered w-28 text-neutral-300">
-        <option disabled selected>
-        Select
-        </option>
-        <option>Eng</option>
-        <option>Bangla</option>
-      </select>
-      
-      {/*  */}
-     
-      <select defaultValue="eng" className="select btn-sm bg-neutral-700 select-bordered w-28 text-neutral-300">
-        <option disabled selected>
-        Select 
-        </option>
-        <option>Eng</option>
-        <option>Bangla</option>
-      </select>
-       <div className="mx-7 text-white">|</div>
-       <button  className="btn-sm btn-neutral mx-3 bg-neutral-700 text-neutral-300">Sign In / Sign Up</button>
+    <div>
+      <div className="flex justify-center items-center ">
+        <select
+          defaultValue="eng"
+          className="select btn-sm bg-neutral-700 select-bordered w-28 text-neutral-300"
+        >
+          <option disabled selected>
+            Select
+          </option>
+          <option>Eng</option>
+          <option>Bangla</option>
+        </select>
+
+        {/*  */}
+
+        <select
+          defaultValue="eng"
+          className="select btn-sm bg-neutral-700 select-bordered w-28 text-neutral-300"
+        >
+          <option disabled selected>
+            Select
+          </option>
+          <option>Eng</option>
+          <option>Bangla</option>
+        </select>
+        <div className="mx-7 text-white">|</div>
+        <button className="btn-sm btn-neutral mx-3 bg-neutral-700 text-neutral-300">
+          Sign In / Sign Up
+        </button>
+      </div>
+      {/* <h1>|</h1> */}
     </div>
-    {/* <h1>|</h1> */}
-   </div>
   );
 }
