5a00ea03692f96f1c5f30888cb97ab97b5adc647
md abu bokor siddik <mdabubokorsiddikbissness@gmail.com>
2025-02-23 22:12:36 +0600

    added some code

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -2,14 +2,14 @@ import React from "react";
 
 const Card = () => {
   return (
-    <div className="card bg-base-100 w-96 shadow-xl">
+    <div className="card bg-base-100 w-72 h-96 shadow-xl">
       <figure>
         <img
           src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
           alt="Shoes"
         />
       </figure>
-      
+
       <div className="card-body">
         <h2 className="card-title">
           Shoes!
