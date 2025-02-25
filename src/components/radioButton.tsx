56d20f685e63d2feb3c459cfe9d8dda830266c8a
md abu bokor siddik <mdabubokorsiddikbissness@gmail.com>
2025-02-25 23:23:03 +0600

    Accordion added some code

--- src/components/radioButton.tsx
+++ src/components/radioButton.tsx
@@ -2,7 +2,6 @@ const RadioButton = () => {
   return (
     <div className="flex items-center ">
       <input type="radio" name="radio-1" className="radio" defaultChecked />
-  
     </div>
   );
 };
