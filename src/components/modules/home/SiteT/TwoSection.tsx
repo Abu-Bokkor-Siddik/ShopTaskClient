701bf70e6ec595c0f23e7ba1a686e2c633425b01
md abu bokor siddik <mdabubokorsiddikbissness@gmail.com>
2025-02-27 22:29:19 +0600

    added some update

--- src/components/modules/home/SiteT/TwoSection.tsx
+++ src/components/modules/home/SiteT/TwoSection.tsx
@@ -1,11 +1,88 @@
-import React from 'react'
+"use client";
+import { useState } from "react";
+import { GoChevronDown, GoChevronUp } from "react-icons/go";
 
-const TwoSection = () => {
+export default function TwoSection() {
+  const [isOpen, setIsOpen] = useState(null);
+  const dataArr = [
+    {
+      title: "All Category",
+      description:
+        'To create an account, click on the "Sign Up" button and fill out the required information. Once done, you can enjoy the benefits of being a registered member.',
+    },
+  ];
+  const toggle = (idx) => {
+    setIsOpen((prevIdx) => (prevIdx === idx ? null : idx));
+  };
+
+ 
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <div className=" h-auto max-w-[400px] rounded-lg bg-zinc-400 p-3 ">
+      {dataArr.map((PerAccordion, idx) => (
+        <div key={idx} className="">
+          <button
+            onClick={() => toggle(idx)}
+            className="flex h-full w-full items-center justify-between font-medium text-white outline-none"
+          >
+            <span className="text-xl ml-28">{PerAccordion.title}</span>
+            <span className="rounded-full p-2">
+              {isOpen === idx ? (
+                <GoChevronUp className="h-10 w-9" />
+              ) : (
+                <GoChevronDown className="h-10 w-9"></GoChevronDown>
+              )}
+            </span>
+          </button>
 
-export default TwoSection
+          <div
+            className={`grid ml-28 -mt-3 items-center text-black overflow-hidden  transition-all duration-300 ease-in-out ${isOpen === idx ? "grid-rows-[1fr] pb-1 pt-3 opacity-100" : "grid-rows-[0fr] opacity-0"}`}
+          >
+            <div className="flex my-2  items-center gap-2">
+            <div className="flex items-center ">
+      <input type="radio" name="radio-1" className="radio" defaultChecked />
+    </div>
+              <h1 className="text-white">Fresh Foods</h1>
+            </div>
+            <div className="flex my-2  items-center gap-2">
+            <div className="flex items-center ">
+      <input type="radio" name="radio-1" className="radio" defaultChecked />
+    </div>
+              <h1 className="text-white">Vegetable</h1>
+            </div>
+            <div className="flex my-2  items-center gap-2">
+              <div className="flex items-center ">
+      <input type="radio" name="radio-1" className="radio" defaultChecked />
+    </div>
+              <h1 className="text-white">Cooking</h1>
+            </div>
+            <div className="flex my-2  items-center gap-2">
+              <div className="flex items-center ">
+      <input type="radio" name="radio-1" className="radio" defaultChecked />
+    </div>
+              <h1 className="text-white">Snacks</h1>
+            </div>
+            <div className="flex my-2  items-center gap-2">
+              <div className="flex items-center ">
+      <input type="radio" name="radio-1" className="radio" defaultChecked />
+    </div>
+              <h1 className="text-white">Beverages</h1>
+            </div>
+            <div className="flex my-2  items-center gap-2">
+              <div className="flex items-center ">
+      <input type="radio" name="radio-1" className="radio" defaultChecked />
+    </div>
+              <h1 className="text-white">Beauty</h1>
+            </div>
+            <div className="flex my-2  items-center gap-2">
+              <div className="flex items-center ">
+      <input type="radio" name="radio-1" className="radio" defaultChecked />
+    </div>
+              <h1 className="text-white">Bread</h1>
+            </div>
+          <hr />
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}
