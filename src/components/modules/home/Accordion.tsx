98ec0b99fb12de97d62396fe7cb24c8860db9682
md abu bokor siddik <mdabubokorsiddikbissness@gmail.com>
2025-02-27 22:26:28 +0600

    added some update

--- src/components/modules/home/Accordion.tsx
+++ src/components/modules/home/Accordion.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { useState } from "react";
 import { GoChevronDown, GoChevronUp } from "react-icons/go";
-import RadioButton from "../../radioButton";
+import RadioButton from "./radioButton";
 export default function BasicAccordion() {
   const [isOpen, setIsOpen] = useState(null);
   const dataArr = [
---
+++ src/components/modules/home/SiteT/TwoSection.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TwoSection = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default TwoSection
