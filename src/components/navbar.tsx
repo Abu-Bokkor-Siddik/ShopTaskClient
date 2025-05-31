04b8fcdd009f8410e3f3e9448ec8579fbdf570e1
md abu bokor siddik <mdabubokorsiddikbissness@gmail.com>
2025-05-31 22:27:32 +0600

    added icon

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -102,7 +102,9 @@ export const Navbar = () => {
         </ul> */}
       </div>
       <div className="navbar-end">
-        <div className="flex justify-center items-center gap-3"><FaRegHeart className="h-6 w-6" /><p >|</p><div className=" btn flex justify-center items-center"><a ><MdOutlineShoppingBag className="w-7 h-7 " /> <p className="absolute ">icon</p></a> <div className="flex flex-col gap-2"><p className="">Shopping Cards</p> <p>$ 454.44</p></div></div></div>
+        <div className="flex justify-center items-center gap-3">
+          <FaRegHeart className="h-6 w-6" /><p >|</p>
+          <div className=" btn relative flex justify-center items-center"><p><MdOutlineShoppingBag className="w-7 h-7 " /> <p className="absolute top-0 bg-pink-500 p-1 rounded-full left-8">4</p></p> <div className="flex flex-col gap-2"><p className=" w-auto opacity-50 h-3">Shopping Cards</p> <p>$ 454.44</p></div></div></div>
       </div>
     </div>
   );
