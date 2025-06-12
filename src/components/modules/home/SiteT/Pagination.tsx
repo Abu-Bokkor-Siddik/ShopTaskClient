b88a745460487b0c010e40154c0b2c481fad7a1c
md abu bokor siddik <mdabubokorsiddikbissness@gmail.com>
2025-06-12 13:49:35 +0600

    added

--- src/components/modules/home/SiteT/Pagination.tsx
+++ src/components/modules/home/SiteT/Pagination.tsx
@@ -1,11 +1,12 @@
-import React from 'react'
-
-const Pagination = () => {
+export default function Pagination() {
   return (
-    <div>
-      
+    <div className="flex justify-center items-center space-x-2 mt-6">
+      <button className="px-3 py-1 rounded bg-green-600 text-white">1</button>
+      <button className="px-3 py-1 rounded bg-gray-200">2</button>
+      <button className="px-3 py-1 rounded bg-gray-200">3</button>
+      <span className="px-3 py-1">...</span>
+      <button className="px-3 py-1 rounded bg-gray-200">21</button>
     </div>
-  )
+  );
 }
 
-export default Pagination
