b4d2237f79a415dcee9cde433e16057988742e3a
md abu bokor siddik <mdabubokorsiddikbissness@gmail.com>
2025-06-14 20:28:01 +0600

    added

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -76,28 +76,7 @@ export const Navbar = () => {
       </div>
       <div className="navbar-center hidden lg:flex">
         <Search></Search> <button>Search</button>
-        {/* <ul className="menu menu-horizontal px-1">
-          
-          <li>
-            <a>Item 1</a>
-          </li>
-          <li>
-            <details>
-              <summary>Parent</summary>
-              <ul className="p-2">
-                <li>
-                  <a>Submenu 1</a>
-                </li>
-                <li>
-                  <a>Submenu 2</a>
-                </li>
-              </ul>
-            </details>
-          </li>
-          <li>
-            <a>Item 3</a>
-          </li>
-        </ul> */}
+      
       </div>
       <div className="navbar-end">
         <div className="flex justify-center items-center gap-3">
