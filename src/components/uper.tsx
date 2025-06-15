b4e16028d7cf1e16ebd1dc7d473ac0f750940e66
md abu bokor siddik <mdabubokorsiddikbissness@gmail.com>
2025-06-15 09:22:28 +0600

    added

--- src/components/uper.tsx
+++ src/components/uper.tsx
@@ -1,13 +1,19 @@
 /* eslint-disable prettier/prettier */
 import { CiLocationOn } from "react-icons/ci";
-import {Autocomplete,AutocompleteSection,AutocompleteItem} from "@heroui/autocomplete";
+
 import Auto from "./autoComplete";
 const Uper = () => {
-  return <div className="w-full h-14 bg-neutral-700 flex justify-between items-center">
-    <div className="text-neutral-300 text-sm ml-16 flex justify-center items-center gap-4 "><CiLocationOn /> <div>Store Location : lincoin- 344,llinois,Chicago,USA</div></div>  <div><Auto></Auto></div>
+  return (
+    <div className="w-full h-14 bg-neutral-700 flex justify-between items-center">
+      <div className="text-neutral-300 text-sm ml-16 flex justify-center items-center gap-4 ">
+        <CiLocationOn />{" "}
+        <div>Store Location : lincoin- 344,llinois,Chicago,USA</div>
+      </div>{" "}
+      <div>
+        <Auto></Auto>
+      </div>
     </div>
-   
-   
+  );
 };
 
 export default Uper;
