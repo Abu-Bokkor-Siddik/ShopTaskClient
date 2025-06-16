766485a1bdccf836331e0f77a4b971daac1eac68
md abu bokor siddik <mdabubokorsiddikbissness@gmail.com>
2025-06-16 23:03:06 +0600

    added

--- src/components/modules/home/SiteT/SaleCard.tsx
+++ src/components/modules/home/SiteT/SaleCard.tsx
@@ -4,7 +4,7 @@ const SaleCard = () => {
   return (
     <div className="card bg-neutral text-neutral-content h-28 w-72">
       <div className="card-body items-center text-center">
-        <h2 className="card-title">Cookies!</h2>
+        <h2 className="card-title">79% </h2>
         <p>We are using cookies for no reason.</p>
       </div>
     </div>
