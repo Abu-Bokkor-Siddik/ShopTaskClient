8a7f1e931dfa1eb220abefc78a6dab776037eca4
md abu bokor siddik <mdabubokorsiddikbissness@gmail.com>
2025-06-18 22:31:17 +0600

    added

--- src/components/modules/home/SiteT/SaleCard.tsx
+++ src/components/modules/home/SiteT/SaleCard.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import React from "react";
 import { FaStar } from "react-icons/fa";
 
@@ -6,17 +7,7 @@ const SaleCard = () => {
     <div className="card bg-neutral text-neutral-content h-28 w-72">
       <div className="card-body items-center text-center">
         <div>
-          <div>
-            <h1>products name </h1>
-            <p className="mb-2">300$</p>
-            <p className="flex gap-1 items-center">
-              <FaStar className="text-yellow-500" />
-              <FaStar className="text-yellow-500" />
-              <FaStar className="text-yellow-500" />
-              <FaStar className="text-yellow-500" />
-              <FaStar className="text-yellow-500" />
-            </p>
-          </div>
+          <Image className="h-14 w-14 rounded-md " src="" alt="images" />
         </div>
       </div>
     </div>
