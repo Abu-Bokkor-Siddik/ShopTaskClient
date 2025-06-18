ab361e967ec5bfe2e704741718aba828709c746c
md abu bokor siddik <mdabubokorsiddikbissness@gmail.com>
2025-06-18 22:38:01 +0600

    added

--- src/components/modules/home/SiteT/SaleCard.tsx
+++ src/components/modules/home/SiteT/SaleCard.tsx
@@ -6,8 +6,8 @@ const SaleCard = () => {
   return (
     <div className="card bg-neutral text-neutral-content h-28 w-72">
       <div className="card-body items-center text-center">
-        <div>
-          <Image className="h-14 w-14 rounded-md " src="" alt="images" />
+        <div className="flex gap-3 items-center">
+          {/* <Image width={200} height={200} className=" rounded-md " src="https://i.ibb.co/j8mhHQ6/download-2.jpg" alt="images" /> */}
           <div>
             <h1>products name </h1>
             <p className="mb-2">300$</p>
