e6ab002bacbaa28b9d8c998f3ca5edd55c8164d9
md abu bokor siddik <mdabubokorsiddikbissness@gmail.com>
2025-06-18 22:32:09 +0600

    added

--- src/components/modules/home/SiteT/SaleCard.tsx
+++ src/components/modules/home/SiteT/SaleCard.tsx
@@ -8,6 +8,17 @@ const SaleCard = () => {
       <div className="card-body items-center text-center">
         <div>
           <Image className="h-14 w-14 rounded-md " src="" alt="images" />
+          <div>
+            <h1>products name </h1>
+            <p className="mb-2">300$</p>
+            <p className="flex gap-1 items-center">
+              <FaStar className="text-yellow-500" />
+              <FaStar className="text-yellow-500" />
+              <FaStar className="text-yellow-500" />
+              <FaStar className="text-yellow-500" />
+              <FaStar className="text-yellow-500" />
+            </p>
+          </div>
         </div>
       </div>
     </div>
