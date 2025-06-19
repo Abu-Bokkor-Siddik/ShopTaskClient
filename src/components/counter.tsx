3d168bd7cfb3de47b94f5db17f5a3f89dfc4687a
md abu bokor siddik <mdabubokorsiddikbissness@gmail.com>
2025-06-19 23:06:59 +0600

    added

--- src/components/counter.tsx
+++ src/components/counter.tsx
@@ -7,8 +7,10 @@ export const Counter = () => {
   const [count, setCount] = useState(0);
 
   return (
-    <Button radius="full" onPress={() => setCount(count + 1)}>
-      Count is {count}
-    </Button>
+    // <Button radius="full" onPress={() => setCount(count + 1)}>
+    //   Count is {count}
+    // </Button>
+    <div>
+      </div>
   );
 };
