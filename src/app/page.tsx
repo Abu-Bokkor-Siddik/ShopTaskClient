c88354f567c2d0b770872052d941d6ae173e7f1b
Md Abu Bokkor Siddik <mdabubokorsiddikbissness@gmail.com>
2025-06-23 11:00:46 -0700

    added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -124,7 +124,7 @@ export default function Home() {
             {/* end */}
             {/* pagination  */}
             <div>
-
+              <Pagination></Pagination>
             </div>
           </div>
         </div>
