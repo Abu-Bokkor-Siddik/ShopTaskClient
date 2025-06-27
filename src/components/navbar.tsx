74c6e506e2deaa4962690341a6a389e9b21b6580
Md Abu Bokkor Siddik <mdabubokorsiddikbissness@gmail.com>
2025-06-27 09:45:30 -0700

    added

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -48,28 +48,6 @@ export const Navbar = () => {
               />
             </svg>
           </div>
-          <ul
-            tabIndex={0}
-            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
-          >
-            <li>
-              <a>Item 1</a>
-            </li>
-            <li>
-              <a>Parent</a>
-              <ul className="p-2">
-                <li>
-                  <a>Submenu 1</a>
-                </li>
-                <li>
-                  <a>Submenu 2</a>
-                </li>
-              </ul>
-            </li>
-            <li>
-              <>Item 3</>
-            </li>
-          </ul>
         </div>
         <p className="btn btn-ghost text-2xl">Ecobazar</p>
       </div>
