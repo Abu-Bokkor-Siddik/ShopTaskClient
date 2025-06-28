4b0361104c53843ebb6d95e14d792c7243ef130c
Md Abu Bokkor Siddik <mdabubokorsiddikbissness@gmail.com>
2025-06-28 09:54:28 -0700

    added

--- src/components/uper.tsx
+++ src/components/uper.tsx
@@ -3,8 +3,8 @@ import { CiLocationOn } from "react-icons/ci";
 import Auto from "./autoComplete";
 const Uper = () => {
   return (
-    <div className="w-full h-14 bg-neutral-700 flex justify-between items-center">
-      <div className="text-neutral-300 text-sm ml-16 flex justify-center items-center gap-4 ">
+    <div className="w-full  h-14 bg-neutral-700 flex justify-between items-center">
+      <div className="text-neutral-300  flex-col text-sm ml-16 flex justify-center items-center gap-4 ">
         <CiLocationOn />{" "}
         <div>Store Location : lincoin- 344,llinois,Chicago,USA</div>
       </div>{" "}
