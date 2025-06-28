e412f58ffbf15d2e327dc1d7b229903426d77250
Md Abu Bokkor Siddik <mdabubokorsiddikbissness@gmail.com>
2025-06-28 10:03:02 -0700

    added

--- src/components/uper.tsx
+++ src/components/uper.tsx
@@ -4,7 +4,7 @@ import Auto from "./autoComplete";
 const Uper = () => {
   return (
     <div className="w-full flex-col lg:flex-row max-h-auto bg-neutral-700 flex justify-between items-center">
-      <div className="text-neutral-300   text-sm ml-16 flex justify-center items-center gap-4 ">
+      <div className="text-neutral-300 text-sm lg:ml-16 flex justify-center items-center gap-4 ">
         <CiLocationOn />{" "}
         <div>Store Location : lincoin- 344,llinois,Chicago,USA</div>
       </div>{" "}
