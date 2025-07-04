d2d826c129acd599ced4c7f7bc4b67ce149dc98a
Md Abu Bokkor Siddik <mdabubokorsiddikbissness@gmail.com>
2025-07-04 10:31:26 -0700

    Design Refector

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -61,7 +61,7 @@ export const Navbar = () => {
             ></label>
             <ul className="menu  bg-base-200 text-base-content h-full w-80 p-4">
               {/* Sidebar content here */}
-              <div className="flex flex-col gap-2">
+              <div className="flex lg:hidden flex-col gap-2">
                 {/* <a
                   className="text-base text-[#06283D]   font-bold"
                   href="#home"
