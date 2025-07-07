72610f4012f1da48816efd3a17ec823d7ab42fd2
Md Abu Bokkor Siddik <mdabubokorsiddikbissness@gmail.com>
2025-07-07 10:46:03 -0700

    Design Refector

--- src/components/modules/home/SiteT/ThreeSection.tsx
+++ src/components/modules/home/SiteT/ThreeSection.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState } from "react";
-import { FaStar } from "react-icons/fa";
+
 import { GoChevronDown, GoChevronUp } from "react-icons/go";
 import Rating from "./Rating";
 export default function ThreeSection() {
