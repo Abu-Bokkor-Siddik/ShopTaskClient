a1f18b1e48fd6933fc0235b72d48afbf6ade02e8
Md Abu Bokkor Siddik <mdabubokorsiddikbissness@gmail.com>
2025-07-07 10:45:05 -0700

    Design Refector

--- src/components/modules/home/SiteT/Rating.tsx
+++ src/components/modules/home/SiteT/Rating.tsx
@@ -1,26 +1,24 @@
-import { FaStar } from "react-icons/fa"
-
+import { FaStar } from "react-icons/fa";
 
 const Rating = () => {
   return (
-    <div  className="flex items-center">
+    <div className="flex items-center">
       <div className="form-control ">
-                <input
-                  type="checkbox"
-                  defaultChecked
-                  className="checkbox checkbox-success"
-                />
-               
-              </div>
-              {/* end */}
-              <FaStar className="text-yellow-500" />
-                <FaStar className="text-yellow-500" />
-                <FaStar className="text-yellow-500" />
-                <FaStar className="text-yellow-500" />
-                <FaStar className="text-yellow-500" />
-                <p>text</p>
+        <input
+          type="checkbox"
+          defaultChecked
+          className="checkbox checkbox-success"
+        />
+      </div>
+      {/* end */}
+      <FaStar className="text-yellow-500" />
+      <FaStar className="text-yellow-500" />
+      <FaStar className="text-yellow-500" />
+      <FaStar className="text-yellow-500" />
+      <FaStar className="text-yellow-500" />
+      <p>text</p>
     </div>
-  )
-}
+  );
+};
 
-export default Rating
+export default Rating;
