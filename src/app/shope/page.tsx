2623103f874286faff3f81497e7a0c58c12f52db
Md Abu Bokkor Siddik <mdabubokorsiddikbissness@gmail.com>
2025-07-13 09:48:38 -0700

    Design Refector

--- src/app/shope/page.tsx
+++ src/app/shope/page.tsx
@@ -1,11 +1,7 @@
-import React from 'react'
+import React from "react";
 
 const Shope = () => {
-  return (
-    <div>
-      shope
-    </div>
-  )
-}
+  return <div>shope page here</div>;
+};
 
-export default Shope
+export default Shope;
