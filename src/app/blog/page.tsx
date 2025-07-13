fa086ab4280e7a86aa7e17391691271ab50c5c40
Md Abu Bokkor Siddik <mdabubokorsiddikbissness@gmail.com>
2025-07-13 09:44:48 -0700

    Design Refector

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -3,7 +3,8 @@ import { title } from "@/src/components/primitives";
 export default function BlogPage() {
   return (
     <div>
-      <h1 className={title()}>Blog</h1>
+      <h1 className={title()}>Bo</h1>
+      <div>hello blog</div>
     </div>
   );
 }
