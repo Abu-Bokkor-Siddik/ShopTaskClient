2440da667027bc44f2e9824b16912593bf1c6f6b
Md Abu Bokkor Siddik <mdabubokorsiddikbissness@gmail.com>
2025-07-13 09:47:54 -0700

    Design Refector

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,26 +1,25 @@
-import LoginPage from "@/src/components/modules/login/LoginPage"
-import { IoIosArrowForward, IoIosHome } from "react-icons/io"
+import LoginPage from "@/src/components/modules/login/LoginPage";
+import { IoIosArrowForward, IoIosHome } from "react-icons/io";
 
 const Login = () => {
   return (
-  
-      <div>
-             <div className="relative">
-               <img
-                 className="w-screen h-40 mx-8 flex justify-center items-center"
-                 src="https://i.ibb.co.com/RkfYBPWN/Green-Fresh-Vegetable-Mobile-Banner-Ad-1.png"
-                 alt=""
-               />
-               <div className="absolute top-16 flex items-center justify-center text-neutral-400 ml-32 gap-1">
-                 <IoIosHome /> <IoIosArrowForward />
-                 Categories <IoIosArrowForward />
-                 <h1>dinamic</h1>
-               </div>
-             </div>
-             {/* end */}
-             <LoginPage></LoginPage>
+    <div>
+      <div className="relative">
+        <img
+          className="w-screen h-40 mx-8 flex justify-center items-center"
+          src="https://i.ibb.co.com/RkfYBPWN/Green-Fresh-Vegetable-Mobile-Banner-Ad-1.png"
+          alt=""
+        />
+        <div className="absolute top-16 flex items-center justify-center text-neutral-400 ml-32 gap-1">
+          <IoIosHome /> <IoIosArrowForward />
+          Categories <IoIosArrowForward />
+          <h1>dinamic</h1>
+        </div>
+      </div>
+      {/* end */}
+      <LoginPage></LoginPage>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
